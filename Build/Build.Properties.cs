f44e3bde82c66a1017e9bc00fd97752356e5b641
Nice3point <nice3point@gmail.com>
2022-02-01 17:20:49 +0300

    Release 2022.1.1

--- Build/Build.Properties.cs
+++ Build/Build.Properties.cs
@@ -14,7 +14,7 @@ partial class Build
         {"2023", "net48"},
     };
 
-    const string PackVersion = "2021.1.40";
+    const string PackVersion = "2022.1.1";
     const string BuildConfiguration = "Release";
     const string ArtifactsFolder = "output";
 
