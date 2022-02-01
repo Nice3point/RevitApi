54f651f76b492fdf93118b45c5b2fa3a400b4a01
Nice3point <nice3point@gmail.com>
2022-02-01 17:18:01 +0300

    Release 2020.2.11

--- Build/Build.Properties.cs
+++ Build/Build.Properties.cs
@@ -14,7 +14,7 @@ partial class Build
         {"2023", "net48"},
     };
 
-    const string PackVersion = "2019.2.11";
+    const string PackVersion = "2020.2.11";
     const string BuildConfiguration = "Release";
     const string ArtifactsFolder = "output";
 
