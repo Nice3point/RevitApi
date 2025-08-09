e48ca10eb6cc219e40f3777c1935a147b7d215ce
Nice3point <nice3point@gmail.com>
2025-08-09 13:23:01 +0300

    Build 23.1.80.30

--- build/Build.Configuration.cs
+++ build/Build.Configuration.cs
@@ -2,7 +2,7 @@
 {
     [Parameter("Publish a new package with a specific DLL name for all versions")] string AssemblyName;
     [Parameter("Publish packages with a specific Release version")] static string ReleaseVersion;
-    [Parameter("Release notes for the publication")] static string ReleaseNotes = "Build 22.1.80.32";
+    [Parameter("Release notes for the publication")] static string ReleaseNotes = "Build 23.1.80.30";
 
     readonly AbsolutePath ArtifactsDirectory = RootDirectory / "output";
     readonly AbsolutePath RootContentDirectory = RootDirectory / "RevitApi" / "Content";
